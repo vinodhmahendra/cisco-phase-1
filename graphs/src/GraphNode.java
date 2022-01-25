472d14c14d46387ba5d9c412f1021f941b49c71b
vinodhmahendra <vinodh.mahendra@gmail.com>
2022-01-25 10:31:46 +0530

    Add files via upload

---
+++ graphs/src/GraphNode.java
@@ -0,0 +1,14 @@
+
+public class GraphNode {
+
+		public String name;
+		public int index;
+		
+		public GraphNode(String name, int index) {
+			super();
+			this.name = name;
+			this.index = index;
+		}
+		
+		
+}
